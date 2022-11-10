9aa16d1d20a1ce186a3174b6826f752024b910c0
fkrein1 <felipekrein@gmail.com>
2022-11-10 20:13:40 -0300

    feat: getimages max to 100

--- backend/src/modules/useCases/getImages/GetImagesUseCase.ts
+++ backend/src/modules/useCases/getImages/GetImagesUseCase.ts
@@ -3,7 +3,7 @@ import { prisma } from '../../../database/prismaClient';
 export class GetImagesUseCase {
   async execute() {
     const images = await prisma.image.findMany({
-      take: 50,
+      take: 100,
       orderBy: { date: 'desc' },
     });
 
