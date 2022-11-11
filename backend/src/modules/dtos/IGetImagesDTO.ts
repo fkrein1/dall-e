c85196525d0e36161615b1f8e24c98527473a2b4
fkrein1 <felipekrein@gmail.com>
2022-11-11 09:59:46 -0300

    fix: zod query schema

--- backend/src/modules/dtos/IGetImagesDTO.ts
+++ backend/src/modules/dtos/IGetImagesDTO.ts
@@ -1,10 +1,7 @@
 import * as z from 'zod';
 
 export const GetImageQuerySchema = z.object({
-  page: z.preprocess(
-    (a) => parseInt(z.string().parse(a), 10),
-    z.number().min(1).max(300),
-  ),
+  page: z.string().regex(/^\d+$/).transform(Number)
 });
 
 export type IGetImagesDTO = z.infer<typeof GetImageQuerySchema>;
