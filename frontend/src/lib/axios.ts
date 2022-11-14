4f2e7fa8e898618a461f1d3782903fb47436eef0
fkrein1 <felipekrein@gmail.com>
2022-11-14 10:24:40 -0300

    feat: api on subdomain

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.PROD
-  ? 'https://dall-e-production.up.railway.app'
+  ? 'https://api.dalleart.com.br'
   : 'http://localhost:3008';
 
 export const api = axios.create({
