6df517ae0838a7629c62165ee1fb5b4cd9dd257a
fkrein1 <felipekrein@gmail.com>
2023-05-05 15:29:21 -0300

    feat: remove custom domain

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.PROD
-  ? 'https://api.dalleart.com.br'
+  ? 'https://dall-e-production.up.railway.app'
   : 'http://localhost:3008';
 
 export const api = axios.create({
