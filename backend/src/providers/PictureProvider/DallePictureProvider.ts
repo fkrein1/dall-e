151f2e80b07c1fb0509768da55db691e728b9f00
Felipe Krein Rocha <felipekrein@gmail.com>
2023-11-09 18:19:33 -0300

    chore: update openai sdk

--- backend/src/providers/PictureProvider/DallePictureProvider.ts
+++ backend/src/providers/PictureProvider/DallePictureProvider.ts
@@ -1,25 +1,26 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 import { AppError } from '../../errors/AppError';
 import { IPictureProvider } from './IProviderImage';
 
 export class DallePictureProvider implements IPictureProvider {
-  private client: OpenAIApi;
+  private client: OpenAI;
 
   constructor() {
-    const configuration = new Configuration({
+    const openai = new OpenAI({
       apiKey: process.env.OPENAI_KEY,
     });
 
-    this.client = new OpenAIApi(configuration);
+    this.client = openai
   }
   async generate(prompt: string) {
-    const result = await this.client.createImage({
+    const result = await this.client.images.generate({
+      model: "dall-e-3",
       prompt,
       n: 1,
       size: '512x512',
     });
 
-    const imageURL = result.data.data[0].url;
+    const imageURL = result.data[0].url;
     if (!imageURL) throw new AppError(404, 'Error generating image');
     return imageURL;
   }
