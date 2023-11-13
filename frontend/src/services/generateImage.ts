8e266b9901034389db3f1208caab08c7a52dca52
Felipe Krein Rocha <felipekrein@gmail.com>
2023-11-13 12:00:48 -0300

    fix: local storage name

--- frontend/src/services/generateImage.ts
+++ frontend/src/services/generateImage.ts
@@ -9,12 +9,12 @@ interface Image {
 export async function generateImage(prompt: string) {
   const currentDate = new Date();
   const dayOfMonth = String(currentDate.getDate());
-  const lastImageDay = localStorage.getItem('day');
+  const lastImageDay = localStorage.getItem('@dall-e-art:day');
   if (dayOfMonth === lastImageDay) {
     return alert('Sorry. You reached your daily limit.');
   }
 
-  localStorage.setItem('day', dayOfMonth);
+  localStorage.setItem('@dall-e-art:day', dayOfMonth);
   const { data } = await api.post<Image>('images', { prompt });
   return data;
 }
